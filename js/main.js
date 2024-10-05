10ff41ac5568361856dd75dde741eb0ddac66657
Liliia <154988375+LiliiaGalkina@users.noreply.github.com>
2024-10-05 18:04:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let burger = document.querySelector('#burger');
+let menu = document.querySelector('#menu');
+
+
+burger.addEventListener('click', func);
+
+function func() {
+   burger.classList.toggle('menu__burger_active');
+	menu.classList.toggle('menu__list_active');
+}
